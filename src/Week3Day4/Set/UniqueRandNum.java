dd0ef9f99f5cc103893abbbbcf6947228c4dcf37
Miji <mjkim2304@gmail.com>
2022-10-06 10:17:19 +0900

    중복 제거

--- src/Week3Day4/Set/UniqueRandNum.java
+++ src/Week3Day4/Set/UniqueRandNum.java
@@ -1,13 +1,19 @@
 package Week3Day4.Set;
 
+import java.util.*;
+
 public class UniqueRandNum {
     public static void main(String[] args) {
         RandomNumberGenerator rng = new RandomNumberGenerator();
+        Set<Integer> unique = new HashSet<>();
         // 50 random numbers
         for (int i = 0; i < 50; i++) {
             // create random numbers within the range of 10
             int randNum = rng.create(10);
-            System.out.println(randNum);
+            if (!unique.contains(randNum)){
+                unique.add(randNum);
+                System.out.println(randNum);
+            }
         }
     }
 }
