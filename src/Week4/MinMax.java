057763c13e9b7ffadcf73137da5ab9f98aa7ce83
Miji <mjkim2304@gmail.com>
2022-10-13 08:22:58 +0900

    MinMax 인터페이스와 콜백

---
+++ src/Week4/MinMax.java
@@ -0,0 +1,50 @@
+package Week4;
+
+interface Compare{
+    boolean doSomething(int valueA, int valueB);
+}
+
+public class MinMax {
+    private int getMaxOrMin(int[] arr, Compare compare) {
+        int targetValue = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            boolean isSth = compare.doSomething(arr[i],targetValue);
+            if (isSth) {
+                targetValue = arr[i];
+            }
+        }
+        return targetValue;
+
+    }
+
+
+    public int max(int[] arr){
+        return getMaxOrMin(arr, new Compare(){
+            @Override
+            public boolean doSomething(int valueA, int valueB){
+                return valueA > valueB;
+            }
+        });
+    }
+
+    public int min(int[] arr){
+        return getMaxOrMin(arr, new Compare(){
+            @Override
+            public boolean doSomething(int valueA, int valueB){
+                return valueA < valueB;
+            }
+        });
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 23, 85, 34, 17, 74, 25, 52, 65};
+
+        MinMax maxAndMin = new MinMax();
+        System.out.println(maxAndMin.max(arr));
+        System.out.println(maxAndMin.min(arr));
+
+
+    }
+}
