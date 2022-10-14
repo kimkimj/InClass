a9e2607827a5af2eeb6054ac61ee7d733426ef71
Miji <mjkim2304@gmail.com>
2022-10-14 10:29:12 +0900

    Insertion Sort

--- src/Week4/InsertionSort.java
+++ src/Week4/InsertionSort.java
@@ -10,18 +10,16 @@ public class InsertionSort {
     }
 
     public static int[] insertionSort(int[] arr){
-        for(int i = 1; i < arr.length; i++){
-            for(int j = i - 1; j == 0; j++) {
-                //
+        for (int i = 1; i < arr.length; i++){
+            for(int j = i; j > 0; j--){
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
             }
         }
         return arr;
     }
-
-    public static void swap(int[] arr, int target, int current){
-        int temp = arr[current];
-        arr[current] = arr[target];
-        arr[target] = temp;
-    }
-
 }
+
