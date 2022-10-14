00f63376a04854f5684cc42143c77d7a0f8af293
Miji <mjkim2304@gmail.com>
2022-10-14 10:00:17 +0900

    Insertion Sort: swap method

--- src/Week4/SelectionSort.java
+++ src/Week4/SelectionSort.java
@@ -9,6 +9,13 @@ public class SelectionSort {
         System.out.println(Arrays.toString(sorted));
     }
 
+    // bublle sort revisited
+    //
+    public static int[] sort(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+
+        }
+    }
     public static int[] selectionSort(int[] arr){
         for(int i = 0; i < arr.length - 1; i++) {
             for(int j = i + 1; j < arr.length; j++){
