29ad52225636b891931025d9be1f6e53cbb750d1
Miji <mjkim2304@gmail.com>
2022-10-17 09:36:10 +0900

    피라미드 별

--- src/Week5/Pyramid.java
+++ src/Week5/Pyramid.java
@@ -4,16 +4,17 @@ public class Pyramid {
     public static void main(String[] args) {
         int numLine = 4;
         for(int i = 0; i < numLine; i++){
-            for(int spaces = 0; spaces < numLine){
-
+            for(int spaces = 0; spaces < numLine - 1 - i; spaces++){
+                System.out.print(" ");
             }
             for(int star = 0; star < 2 * i + 1; star++){
-
+                System.out.print("*");
             }
-            for(int spaces = 0; spaces <
+            for(int spaces = 0; spaces < numLine - 1 - i; spaces++){
+                System.out.print(" ");
+            }
+            System.out.println();
         }
-
-        System.out.println();
     }
 }
 
