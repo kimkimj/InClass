3b232ca21013e2b404f9042ec08cedfc19e6d49e
Miji <mjkim2304@gmail.com>
2022-10-18 09:42:52 +0900

    Printing square with recursion

--- src/Week5/Rectangle.java
+++ src/Week5/Rectangle.java
@@ -2,9 +2,11 @@ package Week5;
 
 public class Rectangle {
     public static void main(String[] args) {
+        /*
         square(4);
         System.out.println();
-        rectangle(4, 6);
+        rectangle(4, 6);*/
+        recursiveSquare(4, 0);
     }
 
     public static void square(int width){
@@ -22,4 +24,16 @@ public class Rectangle {
         }
     }
 
+    public static void recursiveSquare(int width, int count){
+        if (count == width){
+            System.out.print("");
+        } else{
+            for (int i = 0; i < width ; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            recursiveSquare(width, count + 1);
+        }
+
+    }
 }
